b8938d2fa44e55cf0526a7438793da05fba8b956
issiakaboss <ouedross2021@gmail.com>
2026-07-21 18:36:23 +0000

    Ajout explicite du db:seed dans entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ php artisan config:clear
 php artisan cache:clear
 
 # Execution des migrations et seeders
-php artisan migrate:fresh --seed --force
+php artisan migrate:refresh --seed --force
 
 # Démarrage du serveur Laravel
 php artisan serve --host=0.0.0.0 --port=8000
